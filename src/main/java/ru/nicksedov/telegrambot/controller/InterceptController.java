2a7092fe916d41728790f36d9a3664266b5eaab4
Nikolay Sedov <nicksedov@yandex.ru>
2022-11-23 09:34:28 +0300

    Redundant code cleanup

--- src/main/java/ru/nicksedov/telegrambot/controller/InterceptController.java
+++ src/main/java/ru/nicksedov/telegrambot/controller/InterceptController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.nicksedov.telegrambot.pojo.StatusReport;
 import ru.nicksedov.telegrambot.pojo.mail.MailContent;
 import ru.nicksedov.telegrambot.service.TelegramService;
 
--- src/main/java/ru/nicksedov/telegrambot/pojo/TextMessage.java
+++ src/main/java/ru/nicksedov/telegrambot/pojo/TextMessage.java
@@ -3,7 +3,6 @@ package ru.nicksedov.telegrambot.pojo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
