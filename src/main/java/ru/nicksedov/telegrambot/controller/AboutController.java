2fd635d2e24bc7b37057d8be3f48df31dd8a6749
Nikolay Sedov <nicksedov@yandex.ru>
2022-11-24 22:46:09 +0300

    Redundant code cleanup

--- src/main/java/ru/nicksedov/telegrambot/controller/AboutController.java
+++ src/main/java/ru/nicksedov/telegrambot/controller/AboutController.java
@@ -2,10 +2,8 @@ package ru.nicksedov.telegrambot.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
