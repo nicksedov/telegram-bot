fcc99766c5f1c919dde4152e64d6272fa25c529a
Nikolay Sedov <nicksedov@yandex.ru>
2022-12-03 17:22:08 +0300

    Controller for /intercept refactoring

--- src/main/java/ru/nicksedov/telegrambot/controller/APIController.java
+++ src/main/java/ru/nicksedov/telegrambot/controller/APIController.java
@@ -1,12 +1,17 @@
 package ru.nicksedov.telegrambot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.nicksedov.telegrambot.component.MailToTelegramMessageAdapter;
 import ru.nicksedov.telegrambot.component.TelegramService;
+import ru.nicksedov.telegrambot.component.filter.LoggingFilter;
 import ru.nicksedov.telegrambot.pojo.mail.MailContent;
 import ru.nicksedov.telegrambot.pojo.telegram.TelegramMessage;
 
@@ -14,6 +19,8 @@ import java.io.IOException;
 
 @RestController
 public class APIController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
     private final TelegramService telegramService;
 
@@ -30,7 +37,9 @@ public class APIController {
     }
 
     @PostMapping("/intercept")
-    public ResponseEntity<Void> intercept() {
+    public ResponseEntity<Void> intercept(@RequestBody byte[] content) {
+        String base64 = Base64Utils.encodeToString(content);
+        logger.debug("Request body (base64 encoded):\n{}", base64);
         return ResponseEntity.ok().build();
     }
 
